58d35fdc75647704bbcffb72d5224c6b76942cb3
comanoana <oanamariarodila95@yahoo.es>
2021-03-01 12:08:11 +0200

    move to expired/unexpired logic to database side

--- routes/medstore-db.js
+++ routes/medstore-db.js
@@ -21,7 +21,7 @@ router.get("/install", function (req, res, next) {
   pool.getConnection(function (err, connection) {
     if (err) throw err;
     const sql = `
-    CREATE TABLE IF NOT EXISTS drugs (id INT NOT NULL AUTO_INCREMENT, drugName TEXT NOT NULL, category TEXT NOT NULL, expirationDay TEXT NOT NULL, link TEXT NOT NULL, amount TEXT NOT NULL, isExpired BOOLEAN, PRIMARY KEY (id)) ENGINE = InnoDB;
+    CREATE TABLE IF NOT EXISTS drugs (id INT NOT NULL AUTO_INCREMENT, drugName TEXT NOT NULL, category TEXT NOT NULL, expirationDay TEXT NOT NULL, link TEXT NOT NULL, amount TEXT NOT NULL, PRIMARY KEY (id)) ENGINE = InnoDB;
     `;
     connection.query(sql, function (err, results) {
       if (err) throw err;
@@ -31,13 +31,10 @@ router.get("/install", function (req, res, next) {
   });
 });
 
-/**
- *
- */
 router.get("/", function (req, res, next) {
   pool.getConnection(function (err, connection) {
     if (err) throw err;
-    const sql = `SELECT id, drugName, category, expirationDay, link, amount FROM drugs`;
+    const sql = `SELECT * FROM drugs`;
     connection.query(sql, function (err, results) {
       if (err) throw err;
       connection.release();
@@ -46,21 +43,49 @@ router.get("/", function (req, res, next) {
   });
 });
 
-/**
- *
- */
+router.get("/expired", function (req, res, next) {
+const datetime = new Date().toISOString().split('T')[0];
+
+console.log("DATE -> ", datetime);
+
+  pool.getConnection(function (err, connection) {
+    if (err) throw err;
+    const sql = `SELECT * FROM drugs WHERE expirationDay < '${datetime}'`;
+    connection.query(sql, function (err, results) {
+      if (err) throw err;
+      connection.release();
+
+      console.log("RESULT -> ", results);
+      res.json(results);
+    });
+  });
+});
+
+router.get("/unexpired", function (req, res, next) {
+  const datetime = new Date().toISOString().split('T')[0];
+  
+    pool.getConnection(function (err, connection) {
+      if (err) throw err;
+      const sql = `SELECT * FROM drugs WHERE expirationDay > '${datetime}'`;
+      connection.query(sql, function (err, results) {
+        if (err) throw err;
+        connection.release();
+        res.json(results);
+      });
+    });
+  });
+
 router.post("/create", function (req, res, next) {
   const drugName = req.body.drugName;
   const category = req.body.category;
   const expirationDay= req.body.expirationDay;
   const link= req.body.link;
   const amount= req.body.amount;
-  const isExpired = req.body.isExpired;
 
   pool.getConnection(function (err, connection) {
     if (err) throw err;
-    const sql = `INSERT INTO drugs (id, drugName, category, expirationDay, link, amount, isExpired) VALUES (NULL, ?, ?, ?, ?, ?, ?);`;
-    connection.query(sql, [ drugName, category, expirationDay, link, amount, isExpired], function (err, results) {
+    const sql = `INSERT INTO drugs (id, drugName, category, expirationDay, link, amount) VALUES (NULL, ?, ?, ?, ?, ?);`;
+    connection.query(sql, [ drugName, category, expirationDay, link, amount], function (err, results) {
       if (err) throw err;
       const id = results.insertId;
       connection.release();
